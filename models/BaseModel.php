50527dc00cdd13ad977ee7c6ad2fcc655fdc679f
vhgdev <vhghip2905@gmail.com>
2024-11-20 22:54:40 +0700

    basemodel

---
+++ models/BaseModel.php
@@ -0,0 +1,16 @@
+<?php
+//BaseModel dùng để kết nối đến cơ sở dữ liệu
+class BaseModel{
+    //Thuộc tính lưu trữ đối tượng
+    public $conn = null;
+
+    public function __construct(){
+        try{
+            $this -> conn = new PDO("mysql:host=localhost;dbname=duan1;charset=utf8;port=3306", "root" ,"");
+        }catch(PDOException $e){
+            echo "Lỗi kết nối cơ sở dữ liệu" . $e->getMessage();
+        }
+    }
+}
+
+?>\
