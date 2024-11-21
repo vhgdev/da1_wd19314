bdbb6d2347d7a25229a2f79f8b53d75bfd69f13b
minhtuan-bodoi <minhptph53592@gmail.com>
2024-11-21 23:36:03 +0700

    cc

--- index.php
+++ index.php
@@ -5,13 +5,30 @@
 
 require_once "common/function.php";
 //models
-
+require_once "model/BaseModel.php";
+require_once "model/Category.php";
 //controller
 require_once "controllers/HomeController.php";
 
 
 
 $ctl = $_GET['ctl'] ?? "";
+
+//Demo lớp category
+$cate = new Category;
+
+//dữ liệu 
+$data = [
+    'cate_name'=>'msi lỏ',
+    'type' => 0 // type =1 là thú cưng type=0 là sản phẩm
+];
+//Thêm dữ liệu
+// $cate->create($data);
+// cập nhật 
+$cate->update(4 , $data);
+//hiển thị dữ liệu
+echo "<pre>";
+var_dump($cate->all());
 
 match ($ctl) {
     //admin
